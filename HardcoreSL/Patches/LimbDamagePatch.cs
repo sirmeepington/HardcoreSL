ea4601951f2d7429cd6cca1e854578128344621e
Aidan <sir.meepington.1@gmail.com>
2020-12-06 22:39:20 +0000

    Take into account FF for limb damage.
    Didn't expect FF to be on a hardcore server but here we are.

--- HardcoreSL/Patches/LimbDamagePatch.cs
+++ HardcoreSL/Patches/LimbDamagePatch.cs
@@ -16,12 +16,22 @@ namespace HardcoreSL.Patches
             if (target == null || __instance.gameObject == null)
                 return;
 
+            if (!ReferenceHub.TryGetHub(target, out var targetHub) || !ReferenceHub.TryGetHub(__instance.gameObject, out var ourHub))
+                return;
+
+            // Copied basically from HurtPlayer. Prevent limb damage on friendly fire targets.
+            bool ff = targetHub.characterClassManager.Fraction == ourHub.characterClassManager.Fraction;
+            if (ff && !GameCore.ConfigFile.ServerConfig.GetBool("friendly_fire"))
+            {
+                return;
+            }
+
             if (!__instance.TryGetComponent(out PlayerHardcoreComponent instanceComponent) ||
                 !target.TryGetComponent(out PlayerHardcoreComponent targetComponent))
                 return;
 
             // No limb damage on SCPs - with the exception of 049-2.
-            if (!ReferenceHub.TryGetHub(target, out var hub) || (hub.characterClassManager.IsAnyScp() && hub.characterClassManager.CurClass != RoleType.Scp0492))
+            if (targetHub.characterClassManager.IsAnyScp() && targetHub.characterClassManager.CurClass != RoleType.Scp0492)
                 return;
 
             float dis = Vector3.Distance(__instance.camera.transform.position, target.transform.position);
